d0fe4791e08be767ca94effebe623d60292c0691
yayworks <raj@yayworks.com>
2017-07-25 18:19:21 -0500

    Create install-openmpi.sh

---
+++ install-openmpi.sh
@@ -0,0 +1,13 @@
+
+#!/bin/sh
+
+cd /tmp && curl https://www.open-mpi.org/software/ompi/v2.0/downloads/openmpi-${MPI_VERSION}.tar.bz2 |tar xjf -
+
+cd /tmp/openmpi-${MPI_VERSION} && \
+    ./configure --with-cuda=/usr/local/cuda --enable-mpi-thread-multiple \
+        --with-wrapper-ldflags=-Wl,-rpath,/lib --enable-picky --enable-mpi-cxx \
+        --enable-orterun-prefix-by-default -disable-io-romio --prefix=/usr && \
+    make -j2 && \
+    make install
+cd /tmp && \
+    rm -rf /tmp/openmpi-${MPI_VERSION}
