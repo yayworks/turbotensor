169f56cb601f4b1e40d6446be7c13855a06b964a
yayworks <raj@yayworks.com>
2017-07-26 12:09:12 -0500

    Update yb-sw-config.NIMBIX.x8664.turbotensor.sh

--- yb-sw-config.NIMBIX.x8664.turbotensor.sh
+++ yb-sw-config.NIMBIX.x8664.turbotensor.sh
@@ -35,7 +35,7 @@ rm com.out
 y
 EOF
 
-source /root/anaconda3/bin/activate tensorflow
+/root/anaconda3/bin/activate tensorflow
 
 /root/anaconda3/bin/conda install -c conda-forge tensorflow <<EOF
 y
