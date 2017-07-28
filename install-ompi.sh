0608d56feacca3c741b3273996815c9c7fdb2778
yayworks <raj@yayworks.com>
2017-07-28 09:54:44 -0500

    Create install-ompi.sh

---
+++ install-ompi.sh
@@ -0,0 +1,14 @@
+
+#!/bin/sh
+
+wget https://www.open-mpi.org/software/ompi/v2.0/downloads/openmpi-${MPI_VERSION}.tar.bz2
+tar xvf openmpi-${MPI_VERSION}.tar.bz2
+cd /tmp/openmpi-${MPI_VERSION} && \
+    ./configure --with-cuda=/usr/local/cuda && \
+    make -j4 && \
+    make install
+cd /tmp && \
+    rm -rf /tmp/openmpi-${MPI_VERSION}
+
+echo 'LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib' >> /etc/skel/.bashrc
+echo 'PATH=${PATH}:/usr/local/bin' >> /etc/skel/.bashrc
