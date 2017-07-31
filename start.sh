a67d64f590d8339200b4045ead24d48979ed9384
yayworks <raj@yayworks.com>
2017-07-31 12:12:15 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+
+export MPI_HOME=/usr
+export LD_PRELOAD=/usr/lib/libmpi_cxx.so
+export PYTHONHOME=/usr/local/py_distro
+
+#$PYTHONHOME/bin/python /home/nimbix/ptf_examples/MNIST/tf_lenet3.py --train_batch  64  --iterations 1000
+
+
+numnodes=`cat /etc/JARVICE/nodes|wc -l`
+
+mpirun -np $numnodes /usr/local/setup.x
